b436b5e37aef7cad556d2301b301c5e51bd66539
thetylerace13 <tylergemora@yahoo.com>
2017-04-21 22:46:11 -0700

    Basic player model and interaction with hazards. Physics suck.

---
+++ NotEnoughEdges/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed;
+    public float size;
+    public double bounce;
+    public int health;
+
+	// Use this for initialization
+	void Start ()
+    {
+        transform.localScale = new Vector3(size, size, size);
+
+        GetComponent<Rigidbody2D>().gravityScale = speed;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (health <= 0)
+            Destroy(gameObject);
+	}
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Hazard"))
+            health -= col.gameObject.GetComponent<Hazard>().damage;
+    }
+}
