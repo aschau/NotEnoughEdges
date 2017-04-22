e3a6e96c4694e276931422d624d73b1e0cb3c7c3
Andrew Steven Chau <aschau24@gmail.com>
2017-04-22 03:45:01 -0700

    Added Timer.cs and started testing trail renderers

---
+++ NotEnoughEdges/Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    private float currentTime = 0f; //in seconds
+    private Text textTimer;
+    public bool paused = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        textTimer = this.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!paused)
+        {
+            currentTime += Time.deltaTime;
+            textTimer.text = parseTime(currentTime);
+        }
+    }
+
+    public string parseTime(float time)
+    {
+        return string.Format("{0}:{1:00}", (int)time / 60, (int)time % 60);
+    }
+}
