e0b812fd086ce6becf5a7100fd987893da2c7996
Andrew Steven Chau <aschau24@gmail.com>
2017-04-22 05:14:05 -0700

    Added BackgroundMusicControl.cs

---
+++ NotEnoughEdges/Assets/Scripts/UI/BackgroundMusicControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BackgroundMusicControl : MonoBehaviour
+{
+    public static float sliderAmount = 1f;
+    private AudioSource backgroundMusic;
+    private Slider volumeController;
+    private float originalVolume;
+
+    void Awake()
+    {
+
+        this.backgroundMusic = GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>();
+        this.volumeController = this.GetComponent<Slider>();
+        this.originalVolume = this.backgroundMusic.volume;
+        this.backgroundMusic.volume = this.originalVolume * sliderAmount;
+        this.volumeController.value = sliderAmount;
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void setVolume()
+    {
+        sliderAmount = this.volumeController.value;
+        this.backgroundMusic.volume = this.originalVolume * this.volumeController.value;
+    }
+}
