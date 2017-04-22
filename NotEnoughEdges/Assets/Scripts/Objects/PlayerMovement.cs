7aff33ace3fec398a3798a5f5855a69647e77f3d
Edwin Ho <eaho555@gmail.com>
2017-04-22 13:42:14 -0700

    Added Blinking to InvulnFrames

--- NotEnoughEdges/Assets/Scripts/Objects/PlayerMovement.cs
+++ NotEnoughEdges/Assets/Scripts/Objects/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour {
     public float size;
     public float bounce;
     private Rigidbody2D _rigidbody;
+    private SpriteRenderer _spriteRenderer;
     public float invincibleTime;
     private float invincibleTimer = 0.0f;
     public float terminalVelocity;
@@ -14,6 +15,7 @@ public class PlayerMovement : MonoBehaviour {
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Use this for initialization
@@ -44,6 +46,7 @@ public class PlayerMovement : MonoBehaviour {
         if (col.gameObject.CompareTag("Hazard"))
         {
             invincibleTimer = invincibleTime;
+            StartCoroutine(Blink(invincibleTime, 0.2f));
             gameObject.layer = LayerMask.NameToLayer("Invulnerable");
 
             Hazard colHazard = col.gameObject.GetComponent<Hazard>();
@@ -55,6 +58,19 @@ public class PlayerMovement : MonoBehaviour {
 
             if (colHazard.ability == "Bounce")
                 _rigidbody.AddForce((transform.position - col.transform.position).normalized * bounce);
+        }
+    }
+
+    IEnumerator Blink(float time, float blinkInterval)
+    {
+        for (float timeElapsed = 0f; timeElapsed <= time; timeElapsed += Time.deltaTime)
+        {
+            float fragment = timeElapsed / (blinkInterval * 2);
+            if (fragment < blinkInterval)
+                _spriteRenderer.color = Color.clear;
+            else
+                _spriteRenderer.color = Color.white;
+            yield return new WaitForEndOfFrame();
         }
     }
 }
