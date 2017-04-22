aa430e4b96941ce7dfb6608d923a2b775c00eb84
thetylerace13 <tylergemora@yahoo.com>
2017-04-22 16:27:43 -0700

    Replaced player sprites and confirmed enemy types work

--- NotEnoughEdges/Assets/Scripts/Objects/Hazard.cs
+++ NotEnoughEdges/Assets/Scripts/Objects/Hazard.cs
@@ -8,15 +8,50 @@ public class Hazard : MonoBehaviour {
     public static float timer = 10;
     public string ability = "None";
 
-	// Use this for initialization
-	void Start ()
+    private Rigidbody2D _rigidbody;
+    private Vector2 originalVelocity;
+    private float originalAngularVelocity;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start ()
     {
         Destroy(gameObject, timer);
-	}
+
+        MasterGameManager.instance.pauseManager.onPause += FreezeMovement;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
         transform.Translate(Vector3.up * Time.deltaTime * speed);
 	}
+
+    void OnDestroy()
+    {
+        MasterGameManager.instance.pauseManager.onPause -= FreezeMovement;
+    }
+
+    void FreezeMovement(bool isPaused)
+    {
+        if (isPaused)
+        {
+            this.originalVelocity = this._rigidbody.velocity;
+            this.originalAngularVelocity = this._rigidbody.angularVelocity;
+            this._rigidbody.isKinematic = true;
+            this._rigidbody.velocity = Vector2.zero;
+            this._rigidbody.angularVelocity = 0;
+        }
+
+        else
+        {
+            this._rigidbody.isKinematic = false;
+            this._rigidbody.velocity = this.originalVelocity;
+            this._rigidbody.angularVelocity = this.originalAngularVelocity;
+        }
+    }
 }
