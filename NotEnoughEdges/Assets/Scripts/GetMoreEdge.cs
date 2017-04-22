34cd2e3dd52c3e07d55753c4f823ee92407ede26
Roger <roger@dalkefamily.com>
2017-04-22 12:46:52 -0700

    Added item to get more edges

---
+++ NotEnoughEdges/Assets/Scripts/GetMoreEdge.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GetMoreEdge : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player")) //Hit player
+        {
+            col.GetComponent<ShapeManager>().GainEdge();
+
+            Destroy(gameObject);
+        }
+    }
+}
