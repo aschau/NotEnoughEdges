fd4b9c059eaa514f1b4656e313e26769d75752c7
Roger <roger@dalkefamily.com>
2017-04-22 16:00:33 -0700

    Cleaned up Edge Maker

--- NotEnoughEdges/Assets/Scripts/EdgeMaker.cs
+++ NotEnoughEdges/Assets/Scripts/EdgeMaker.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EdgeMaker : MonoBehaviour
 {
@@ -7,42 +6,31 @@ public class EdgeMaker : MonoBehaviour
     private EdgeCollider2D currentEdge;
     private Vector2[] currentPoints = new Vector2[2];
 	
-	void Start ()
-    {
-		
-	}
-	
 	void Update ()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            //Debug.Log(GetMousePos());
             currentEdge = Instantiate(drawnEdgePrefab);
             currentEdge.transform.SetParent(Camera.main.transform);
-
-            //Debug.Log(currentEdge.points[0]);
+            
             currentPoints[0] = GetLocalMousePos();
-            //Debug.Log(currentEdge.points[0]);
         }
         if (Input.GetMouseButton(0))
         {
-            //Debug.Log(GetMousePos());
-            //Debug.Log(currentEdge.points[1]);
             currentPoints[1] = GetLocalMousePos();
-            //Debug.Log(currentEdge.points[1]);
             
             Vector3[] currentPointsV3 = System.Array.ConvertAll<Vector2,Vector3>(currentPoints, Vector2to3);
             currentEdge.GetComponent<LineRenderer>().SetPositions(currentPointsV3);
         }
         if (Input.GetMouseButtonUp(0))
         {
-            //Debug.Log(GetMousePos());
-            //Debug.Log(currentEdge.points[1]);
             currentPoints[1] = GetLocalMousePos();
-            //Debug.Log(currentEdge.points[1]);
 
             currentEdge.transform.SetParent(null);
             currentEdge.points = currentPoints;
+
+            Vector3[] currentPointsV3 = System.Array.ConvertAll<Vector2, Vector3>(currentPoints, Vector2to3);
+            currentEdge.GetComponent<LineRenderer>().SetPositions(currentPointsV3);
         }
     }
 
