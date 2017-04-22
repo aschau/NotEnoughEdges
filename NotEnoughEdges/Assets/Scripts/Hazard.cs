4ff5eed82a6a0a9c61aad204faca47bb8087d9b3
thetylerace13 <tylergemora@yahoo.com>
2017-04-21 22:22:17 -0700

    Basic enemy scripts and prefab

---
+++ NotEnoughEdges/Assets/Scripts/Hazard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour {
+    public float speed = 1.0f;
+    public int damage = 1;
+    public static double upperBound = 100.0;
+    public string ability = "None";
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(0, speed * Time.deltaTime, 0);
+
+        if (transform.position.y > upperBound)
+            Destroy(gameObject);
+	}
+}
