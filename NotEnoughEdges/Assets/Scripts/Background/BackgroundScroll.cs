11fa11a8cd37826fd129092260b1240233e91a76
Andrew Steven Chau <aschau24@gmail.com>
2017-04-22 03:01:23 -0700

    Added Background/BackgroundScroll.cs.
    Still not sure how we're really going to work the lighting aesthetically.

---
+++ NotEnoughEdges/Assets/Scripts/Background/BackgroundScroll.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    private MeshRenderer background1, background2;
+    private GameObject player;
+
+    void Awake()
+    {
+        this.player = GameObject.FindGameObjectWithTag("Player");
+        this.background1 = GameObject.Find("Background 1").GetComponent<MeshRenderer>();
+        this.background2 = GameObject.Find("Background 2").GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.player.transform.position.y < this.background2.transform.position.y)
+        {
+            this.background1.transform.position = new Vector3(this.background2.transform.position.x,
+                this.background2.transform.position.y - this.background2.bounds.size.y, this.background2.transform.position.z);
+        }
+        if (this.player.transform.position.y < this.background1.transform.position.y)
+        {
+            this.background2.transform.position = new Vector3(this.background1.transform.position.x,
+                this.background1.transform.position.y - this.background1.bounds.size.y, this.background1.transform.position.z);
+        }
+
+
+
+    }
+}
---
+++ NotEnoughEdges/Assets/Scripts/Background/LightToggle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightToggle : MonoBehaviour {
+    private Light lt;
+	// Use this for initialization
+	void Start () {
+        lt = this.transform.parent.GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        lt.enabled = true;
+        lt.color = new Color((float)Random.Range(0, 255) / 255f, (float)Random.Range(0, 255) / 255f, (float)Random.Range(0, 255) / 255f, 1);
+    }
+}
