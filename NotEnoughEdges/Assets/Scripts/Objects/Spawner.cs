b056e7cf65c9c94337ce0e7152ac440ff5c0607d
thetylerace13 <tylergemora@yahoo.com>
2017-04-22 22:13:13 -0700

    Changed some level balancing

--- NotEnoughEdges/Assets/Scripts/Objects/Spawner.cs
+++ NotEnoughEdges/Assets/Scripts/Objects/Spawner.cs
@@ -58,28 +58,28 @@ public class Spawner : MonoBehaviour {
         switch (sm.edgeNum)
         {
             case 3:
-                probList = new List<float> { 1.0f, 0.0f, 0.0f, 0.5f, 0.0f, 0.0f, 2.0f };
+                probList = new List<float> { 1.0f, 0.0f, 0.0f, 0.5f, 0.0f, 0.0f, 1.0f };
                 break;
             case 4:
-                probList = new List<float> { 1.0f, 0.2f, 0.0f, 0.5f, 0.0f, 0.0f, 2.0f };
+                probList = new List<float> { 1.0f, 0.2f, 0.0f, 0.5f, 0.0f, 0.0f, 1.0f };
                 break;
             case 5:
-                probList = new List<float> { 1.0f, 0.3f, 0.0f, 0.6f, 0.0f, 0.0f, 2.0f };
+                probList = new List<float> { 1.0f, 0.3f, 0.0f, 0.6f, 0.0f, 0.0f, 1.0f };
                 break;
             case 6:
-                probList = new List<float> { 1.0f, 0.4f, 0.1f, 0.7f, 0.0f, 0.0f, 2.0f };
+                probList = new List<float> { 1.0f, 0.4f, 0.1f, 0.7f, 0.0f, 0.0f, 1.0f };
                 break;
             case 7:
-                probList = new List<float> { 1.0f, 0.5f, 0.2f, 0.8f, 0.2f, 0.0f, 2.0f };
+                probList = new List<float> { 1.0f, 0.5f, 0.2f, 0.8f, 0.2f, 0.0f, 1.0f };
                 break;
             case 8:
-                probList = new List<float> { 1.0f, 0.6f, 0.3f, 0.9f, 0.4f, 0.5f, 2.0f };
+                probList = new List<float> { 1.0f, 0.6f, 0.3f, 0.9f, 0.4f, 0.5f, 1.0f };
                 break;
             case 9:
-                probList = new List<float> { 1.0f, 0.7f, 0.4f, 1.0f, 0.6f, 1.0f, 2.0f };
+                probList = new List<float> { 1.0f, 0.7f, 0.4f, 1.0f, 0.6f, 1.0f, 1.0f };
                 break;
             case 10:
-                probList = new List<float> { 1.0f, 0.8f, 0.5f, 1.0f, 0.8f, 1.5f, 2.0f };
+                probList = new List<float> { 1.0f, 0.8f, 0.5f, 1.0f, 0.8f, 1.5f, 1.0f };
                 break;
         }
 	}
