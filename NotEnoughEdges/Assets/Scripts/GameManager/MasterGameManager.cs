57029ab35fc45bbd5e87a7388f87d2f382368d6a
Edwin Ho <eaho555@gmail.com>
2017-04-23 16:51:32 -0700

    Added hiscores

--- NotEnoughEdges/Assets/Scripts/GameManager/MasterGameManager.cs
+++ NotEnoughEdges/Assets/Scripts/GameManager/MasterGameManager.cs
@@ -9,12 +9,16 @@ public class MasterGameManager : MonoBehaviour {
     public SceneManagerWrapper sceneManager;
     public PauseManager pauseManager;
     public UIManager uiManager;
+    public SaveManager saveManager;
 
     public bool isGameOver { get; private set; }
+    public float currentTime { get; private set; }
+    public int maxEdges { get; private set; }
 
     PlayerHealth playerHealth;
+    ShapeManager shapeManager;
     GameOverMenu gameOverMenu;
-    WinMenu winMenu;
+    //WinMenu winMenu;
 
     void Awake()
     {
@@ -37,27 +41,47 @@ public class MasterGameManager : MonoBehaviour {
         isGameOver = false;
 
         playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+        shapeManager = playerHealth.GetComponent<ShapeManager>();
         gameOverMenu = GameObject.Find("Game Over Menu").GetComponent<GameOverMenu>();
-        winMenu = GameObject.Find("Win Menu").GetComponent<WinMenu>();
+        //winMenu = GameObject.Find("Win Menu").GetComponent<WinMenu>();
+        shapeManager.onEdgeChange += UpdateMaxEdges;
         playerHealth.onDeath += GameOver;
         playerHealth.onWin += MaxEdge;
+        currentTime = 0f;
+        maxEdges = 3;
+    }
+
+    void Update()
+    {
+        if (!pauseManager.isPaused)
+        {
+            this.currentTime += Time.deltaTime;
+        }
+    }
+
+    void UpdateMaxEdges(int amount)
+    {
+        if (shapeManager.edgeNum > maxEdges)
+            maxEdges = shapeManager.edgeNum;
     }
 
     void GameOver()
     {
         _EndGame();
-        gameOverMenu.EnableGameOverPanel(true);
+        gameOverMenu.ShowGameOver();
     }
 
     void MaxEdge() //AKA win
     {
         _EndGame();
-        winMenu.EnableWinPanel(true);
+        gameOverMenu.ShowWin();
+        //winMenu.EnableWinPanel(true);
     }
 
     void _EndGame()
     {
         Time.timeScale = 0;
         isGameOver = true;
+        saveManager.SaveScore();
     }
 }
---
+++ NotEnoughEdges/Assets/Scripts/GameManager/SaveManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+
+    private int topScoreNum = 3;
+    public List<KeyValuePair<int, float>> hiscoreList { get; private set; }//= new List<KeyValuePair<int, float>>();
+
+    void Start()
+    {
+        LoadScore();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            Debug.Log("Deleting PlayerPrefs");
+            PlayerPrefs.DeleteAll();
+        }
+    }
+
+    public void SaveScore()
+    {
+        float timeScore = MasterGameManager.instance.currentTime;
+        int edgeScore = MasterGameManager.instance.maxEdges;
+
+        KeyValuePair<int, float> newScore = new KeyValuePair<int, float>(edgeScore, timeScore);
+
+        hiscoreList.Add(newScore);
+
+        hiscoreList.Sort(CompareScore);
+
+        for (int i = 0; i < hiscoreList.Count; i++)
+        {
+            string edgeKey = string.Format("@LeaderBoard: Edge Score #{0}", i);
+            string timeKey = string.Format("@LeaderBoard: Time Score #{0}", i);
+            int edgeValue = hiscoreList[i].Key;
+            float timeValue = hiscoreList[i].Value;
+
+            PlayerPrefs.SetInt(edgeKey, edgeValue);
+            PlayerPrefs.SetFloat(timeKey, timeValue);
+            //Debug.LogFormat("Saving Score #{0}: Edges={1}, Time={2}", i, edgeValue, timeValue);
+        }
+
+        PlayerPrefs.Save();
+    }
+
+    public void LoadScore()
+    {
+        hiscoreList = new List<KeyValuePair<int, float>>();
+
+        for (int i = 0; i < topScoreNum; i++)
+        {
+            string edgeKey = string.Format("@LeaderBoard: Edge Score #{0}", i);
+            string timeKey = string.Format("@LeaderBoard: Time Score #{0}", i);
+            int edgeScore = 0;
+            float timeScore = 0;
+
+            if (PlayerPrefs.HasKey(edgeKey) && PlayerPrefs.HasKey(timeKey))
+            {
+                edgeScore = PlayerPrefs.GetInt(edgeKey);
+                timeScore = PlayerPrefs.GetFloat(timeKey);
+                hiscoreList.Add(new KeyValuePair<int, float>(edgeScore, timeScore));
+                //Debug.LogFormat("Loading Score #{0}: Edges={1}, Time={2}", i, edgeScore, timeScore);
+            }
+        }
+    }
+
+    int CompareScore(KeyValuePair<int, float> score1, KeyValuePair<int, float> score2)
+    {
+        int keyCompare = score2.Key.CompareTo(score1.Key);
+        if (keyCompare == 0)
+        {
+            return score1.Value.CompareTo(score2.Value);
+        }
+        else
+        {
+            return keyCompare;
+        }
+    }
+}
