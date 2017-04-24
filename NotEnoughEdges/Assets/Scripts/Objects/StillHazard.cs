1e9264fcae3cf97e05416bc2688d5acc039c2911
thetylerace13 <tylergemora@yahoo.com>
2017-04-23 17:03:01 -0700

    Animation metas + start of shooting animations

--- NotEnoughEdges/Assets/Scripts/Objects/StillHazard.cs
+++ NotEnoughEdges/Assets/Scripts/Objects/StillHazard.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StillHazard : MonoBehaviour {
-    private GameObject theTarget;
     public GameObject projectile;
     public float stillTime;
     public float fireRate;
+    private GameObject theTarget;
     private float stillTimer = 0.0f;
     private bool isClose = false;
     private float fireTimer = 0.0f;
     private Rigidbody2D playerRigidBody;
+    private int numOfFires = 0;
+    private Animator _animator;
 
     void Awake()
     {
         theTarget = GameObject.FindWithTag("Player");
 
         playerRigidBody = theTarget.GetComponent<Rigidbody2D>();
+
+        _animator = GetComponentInChildren<Animator>();
     }
 
     // Use this for initialization
@@ -45,10 +49,30 @@ public class StillHazard : MonoBehaviour {
             if (fireTimer > 1/fireRate) // Fire projectile
             {
                 fireTimer = 0;
-                Instantiate(projectile, transform.position, Quaternion.identity);
+                Fire();
             }
         }
 
         transform.rotation = Quaternion.LookRotation(Vector3.forward, theTarget.transform.position - transform.position  + new Vector3(0, playerRigidBody.velocity.y * 1.5f, 0));
+    }
+
+    void Fire ()
+    {
+        if (numOfFires == 0)
+        {
+            ++numOfFires;
+            _animator.Play("1st Shot Part 1");
+            while (_animator.GetCurrentAnimatorStateInfo(1).IsName("1st Shot Part 1"));
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            _animator.Play("1st Shot Part 2");
+        }
+        else if (numOfFires == 1)
+        {
+            ++numOfFires;
+            _animator.Play("2nd Shot Part 1");
+            while (_animator.GetCurrentAnimatorStateInfo(1).IsName("2nd Shot Part 1")) ;
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            _animator.Play("2nd Shot Part 2");
+        }
     }
 }
