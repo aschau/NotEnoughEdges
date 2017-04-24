fad112029715dc4758c8ca247de77816e4c124ee
thetylerace13 <tylergemora@yahoo.com>
2017-04-23 23:15:43 -0700

    Thorns now pop up along walls to indicate harm

---
+++ NotEnoughEdges/Assets/Scripts/Background/EnableThorns.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableThorns : MonoBehaviour {
+    private PlayerMovement playerMovement;
+    private GameObject[] thorns;
+    private bool activated = false;
+
+    private void Awake()
+    {
+        playerMovement = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+        thorns = GameObject.FindGameObjectsWithTag("Thorns");
+        Debug.Log(thorns.Length);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        foreach (GameObject g in thorns)
+            g.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (playerMovement.wallsHurt && !activated)
+        {
+            foreach (GameObject g in thorns)
+                g.SetActive(true);
+            activated = true;
+        }
+	}
+}
